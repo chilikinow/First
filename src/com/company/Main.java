d57fd9318473585fb96c2f4ee9b44dcefbbe3438
chilikinow <chilikinow@gmail.com>
2020-05-04 20:40:22 +0300

    Add massage Hello world

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello world!");// write your code here
+	System.out.println("Hello world!");
+	System.out.println("I did it!");// write your code here
     }
 }
