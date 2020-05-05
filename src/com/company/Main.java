f2004cc9547989439a6a2f4976decba7902bdb5e
chilikinow <chilikinow@gmail.com>
2020-05-05 18:45:37 +0300

    New version of program Hello world

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,7 +3,9 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	System.out.println("Hello world!");
-	System.out.println("I did it!");// write your code here
+	String a = "Hello";
+	for (int i = 5; i > 0; i--) {
+	    System.out.println(i);
+    }System.out.println(a + " world!");
     }
 }
