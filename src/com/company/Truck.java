2febd2f98094e782108d06db3da164b3b397eaa3
chilikinow <chilikinow@gmail.com>
2020-05-09 14:49:53 +0300

    New version of program Hello world

--- src/com/company/Truck.java
+++ src/com/company/Truck.java
@@ -1,6 +1,7 @@
 package com.company;
 
 public class Truck {
+
     int length, width, height, weight;
 
     public Truck(int length, int width, int height, int weight){
@@ -10,11 +11,4 @@ public class Truck {
         this.weight = weight;
     }
 
-    public int getVolume(Truck truck) {
-        return truck.length * truck.width * truck.height;
-    }
-    public static void main (String [] args){
-        Truck kamaz = new Truck(10, 4, 2, 10);
-        System.out.println(kamaz.getVolume(kamaz));
-    }
 }
