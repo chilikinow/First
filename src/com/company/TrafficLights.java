d90583a73926fde42a89ed10c9912cd993d46a3d
chilikinow <chilikinow@gmail.com>
2020-05-17 14:15:59 +0300

    Svetofor

---
+++ src/com/company/TrafficLights.java
@@ -0,0 +1,63 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class TrafficLights {
+
+
+
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String s = reader.readLine();
+        double a = Double.parseDouble(s);
+        String color = "зеленый";
+        double colorCountGreen = 0;
+        double colorCountYellow = 0;
+        double colorCountRed = 0;
+        int colorFlag = 1;
+        for (double i = 0; i <= a+0.2; i += 0.1) {
+            switch (colorFlag){
+                case (1):
+                    if (colorCountGreen < 3) {
+                        color = "зеленый";
+                        colorCountGreen += 0.1;
+                        System.out.println("i: " + i);
+                        System.out.println("CG: " + colorCountGreen);
+                        System.out.println("Flag: " + colorFlag);
+                    } else {
+                        colorCountGreen = 0;
+                        colorFlag = 2;
+                    }
+                break;
+                case (2):
+                    if (colorCountYellow < 1) {
+                        color = "желтый";
+                        colorCountYellow += 0.1;
+                        System.out.println("i: " + i);
+                        System.out.println("CY: " + colorCountYellow);
+                        System.out.println("Flag: " + colorFlag);
+                    } else {
+                        colorCountYellow = 0;
+                        colorFlag = 3;
+                    }
+                    break;
+                case (3):
+                    if (colorCountRed < 1) {
+                        color = "красный";
+                        colorCountRed += 0.1;
+                        System.out.println("i: " + i);
+                        System.out.println("CR: " + colorCountRed);
+                        System.out.println("Flag: " + colorFlag);
+                    } else {
+                        colorCountRed = 0;
+                        colorFlag = 1;
+                    }
+                    break;
+            }
+        }
+    System.out.println(color);
+    }
+}
--- src/com/company/TrafficLights2.java
+++ src/com/company/TrafficLights2.java
@@ -5,10 +5,10 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-public class Solution {
+public class TrafficLights2 {
 
     public static String color;
-    public static double colorCountGreen = 0.1, colorCountYellow = 0.1, colorCountRed = 0.1;
+    public static double colorCountGreen = 0, colorCountYellow = 0, colorCountRed = 0;
     public static int colorFlag = 1;
 
     public static void main(String[] args) throws IOException {
@@ -17,37 +17,37 @@ public class Solution {
         double a = Double.parseDouble(s);
 
 
-        for (double i = 0; i < a; i += 0.1) {
+        for (double i = 0; i <= a; i += 0.1) {
             System.out.println("i: " + i);
              if ((colorCountGreen < 3) & (colorFlag == 1)) {
-                 Solution.color = "зеленый";
-                 colorCountGreen+= 0.1;
+                 TrafficLights2.color = "зеленый";
                  System.out.println("CG: " + colorCountGreen);
+                 colorCountGreen+= 0.1;
              }
              else {
-                 colorCountGreen = 0.1;
+                 colorCountGreen = 0;
                  colorFlag = 2;
 
                  if ((colorCountYellow < 1) & (colorFlag == 2)) {
-                     Solution.color = "желтый";
+                     TrafficLights2.color = "желтый";
                      colorCountYellow += 0.1;
                      System.out.println("CY: " + colorCountYellow);
                  } else {
-                     colorCountYellow = 0.1;
+                     colorCountYellow = 0;
                      colorFlag = 3;
 
                      if ((colorCountRed < 1) & (colorFlag == 3)) {
-                         Solution.color = "красный";
+                         TrafficLights2.color = "красный";
                          colorCountRed+=0.1;
                          System.out.println("CR: " + colorCountRed);
                      } else {
-                         colorCountRed = 0.1;
+                         colorCountRed = 0;
                          colorFlag = 1;
                      }
                  }
              }
         }
-        System.out.println(Solution.color);
+        System.out.println(TrafficLights2.color);
 
     }
 
