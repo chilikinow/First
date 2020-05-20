7b54f027e4b2fbfd467e90326364a9c963e235f0
chilikinow <chilikinow@gmail.com>
2020-05-20 21:40:45 +0300

    Svetofor

--- src/com/company/TrafficLights2.java
+++ src/com/company/TrafficLights2.java
@@ -9,37 +9,40 @@ public class TrafficLights2 {
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
         String s = reader.readLine();
-        double a = Double.parseDouble(s);
+        double a1 = Double.parseDouble(s);
+        int a = (int) a1;
 
         String color = null;
         double countGreen = 0;
         double countYellow = 0;
         double colorCountRed = 0;
         int colorFlag = 1;
+        int i= 0;
 
-        for (double i = 0; i <= a; i += 0.1) {
+        while (i <= a) {
              if ((countGreen < 3) & (colorFlag == 1)) {
                  color = "зеленый";
-                 countGreen += 0.1;
+                 countGreen++;
              }
              else {
                  countGreen = 0;
                  colorFlag = 2;
                  if ((countYellow < 1) & (colorFlag == 2)) {
                      color = "желтый";
-                     countYellow += 0.1;
+                     countYellow++;
                  } else {
                      countYellow = 0;
                      colorFlag = 3;
                      if ((colorCountRed < 1) & (colorFlag == 3)) {
                          color = "красный";
-                         colorCountRed+=0.1;
+                         colorCountRed++;
                      } else {
                          colorCountRed = 0;
                          colorFlag = 1;
                      }
                  }
              }
+            i++;
         }
         System.out.println(color);
     }
